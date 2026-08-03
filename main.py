9a0cda8c529cb6157a96fc4fc7d3a23ef6b375cf
Louise <louisewang02@gmail.com>
2026-08-04 06:07:41 +1200

    Added an opponent ai

--- main.py
+++ main.py
@@ -30,6 +30,15 @@ def player_animation():
     if player.bottom >= screen_height:
         player.bottom = screen_height
 
+def opponent_ai():
+    if opponent.top < ball.y:
+        opponent.top += opponent_speed
+    if opponent.bottom > ball.y:
+        opponent.bottom -= opponent_speed
+    if opponent.top <= 0:
+        opponent.top = 0
+    if opponent.bottom >= screen_height:
+        opponent.bottom = screen_height
 
 #General setup
 #required to initialize pygame
@@ -55,6 +64,7 @@ light_grey = (200, 200, 200)
 ball_speed_x = 7
 ball_speed_y = 7
 player_speed = 0
+opponent_speed = 7
 
 while True:
     #handling input
@@ -76,7 +86,7 @@ while True:
 
     ball_animation()
     player_animation()
-
+    opponent_ai()
     #visuals
     screen.fill(bg_color)
     pygame.draw.rect(screen, light_grey, player)
